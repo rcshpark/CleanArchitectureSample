ae2644439152a85e67963d3b6dcb6136165e0828
rcshpark <rcshpark@gmail.com>
2025-10-19 12:37:35 +0900

    feat: create local todo repository implement

--- CleanArchitectureSample/Data/Repository/TodoRepositoryImpl.swift
+++ CleanArchitectureSample/Data/Repository/TodoRepositoryImpl.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+final class TodoRepositoryImpl: TodoRepository {
+    private let localDataSource: LocalTodoDataSource
+
+    init(localDataSource: LocalTodoDataSource) {
+        self.localDataSource = localDataSource
+    }
+
+    func fetchTodos() -> [Todo] {
+        localDataSource.getAll()
+    }
+
+    func addTodo(title: String) {
+        localDataSource.insert(title: title)
+    }
+
+    func toggleTodo(id: UUID) {
+        localDataSource.toggle(id: id)
+    }
+}
