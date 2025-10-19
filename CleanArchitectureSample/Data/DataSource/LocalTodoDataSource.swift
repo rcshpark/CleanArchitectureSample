0fe750f08e8498c711a7eaaec52d04d126a3b800
rcshpark <rcshpark@gmail.com>
2025-10-19 12:37:02 +0900

    feat: create local todo datasource

--- CleanArchitectureSample/Data/DataSource/LocalTodoDataSource.swift
+++ CleanArchitectureSample/Data/DataSource/LocalTodoDataSource.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class LocalTodoDataSource {
+    private var todos: [Todo] = []
+
+    func getAll() -> [Todo] {
+        todos
+    }
+
+    func insert(title: String) {
+        todos.append(Todo(id: UUID(), title: title, isComplete: false))
+    }
+
+    func toggle(id: UUID) {
+        guard let index = todos.firstIndex(where: { $0.id == id }) else { return }
+        todos[index].isComplete.toggle()
+    }
+}
