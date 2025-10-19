e8741534bc300f1cce706d10ca232bcd66b1a1bc
rcshpark <rcshpark@gmail.com>
2025-10-19 10:09:58 +0900

    feat: create todo viewmodel

--- CleanArchitectureSample/Presentation/ViewModels/TodoViewModel.swift
+++ CleanArchitectureSample/Presentation/ViewModels/TodoViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by 박상훈 on 10/14/25.
 //
 
-import Foundation
+import SwiftUI
+
+final class TodoViewModel: ObservableObject {
+    @Published var todos: [Todo] = []
+    
+    private let getTodoUseCase: GetTodoUseCase
+    private let addTodoUseCase: AddTodoUseCase
+    private let toggleTodoUseCase: ToggleTodoUseCase
+    
+    init(getTodoUseCase: GetTodoUseCase, addTodoUseCase: AddTodoUseCase, toggleTodoUseCase: ToggleTodoUseCase) {
+        self.getTodoUseCase = getTodoUseCase
+        self.addTodoUseCase = addTodoUseCase
+        self.toggleTodoUseCase = toggleTodoUseCase
+    }
+    
+    func fetchTodos() {
+        todos = getTodoUseCase.execute()
+    }
+    
+    func addTodos(title: String) {
+        addTodoUseCase.execute(title: title)
+        fetchTodos()
+    }
+    
+    func toggleTodo(id: UUID) {
+        toggleTodoUseCase.execute(id: id)
+        fetchTodos()
+    }
+}
